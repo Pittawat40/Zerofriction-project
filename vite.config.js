c2121229666cd38934f3d59ed65cde9381602a04
PITTAWAT <dev@dosetech.co>
2025-06-26 11:04:27 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
     }),
   ],
   define: { 'process.env': {} },
+  base: "/Zerofriction-project/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
